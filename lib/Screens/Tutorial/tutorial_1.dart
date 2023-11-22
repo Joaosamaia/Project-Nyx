e407c16e279199bdadc313db23f6162e16382fda
JonathanEmanoel <138996871+JonathanEmanoel@users.noreply.github.com>
2023-11-22 19:03:54 -0300

    Splash + tutorial routing update
    Updated the tutorial screens and added a landing page, also corrected the routing from the splash screen.
    
    João L. S. A. M

--- lib/Screens/Tutorial/tutorial_1.dart
+++ lib/Screens/Tutorial/tutorial_1.dart
@@ -12,87 +12,89 @@ class _Tutorial1 extends State<Tutorial1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: MainColor.primaryColor,
+        backgroundColor: MainColor.primaryColor,
         body: Padding(
           padding: const EdgeInsets.all(4.5),
           child: Center(
               child: Column(children: [
-                const Text(
+            const SizedBox(height: 30.0),
+            const Text(
               'Bem-vinda',
               style: TextStyle(
                 fontSize: 17.0,
                 color: Colors.black,
               ),
             ),
-                const Text(
-                'Bem-vindas ao NYX: Sua voz contra a violência doméstica!',
-                style: TextStyle(
-                  fontSize: 17.0,
-                  color: Colors.black,
-                ),
+            const Text(
+              'Bem-vindas ao NYX: Sua voz contra a violência doméstica!',
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.black,
               ),
-              const Text(
-                'O NYX é um aplicativo projetado para\nauxiliar as mulheres a não se calarem\ndiante da violência doméstica.',
-                style: TextStyle(
-                  fontSize: 17.0,
-                  color: Colors.black,
-                ),
+            ),
+            const Text(
+              'O NYX é um aplicativo projetado para\nauxiliar as mulheres a não se calarem\ndiante da violência doméstica.',
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.black,
               ),
-              const Text(
-                'Com esta ferramenta, você pode denunciar casos\nde violência  doméstica  de maneira camuflada,\nrápida e segura.',
-                style: TextStyle(
-                  fontSize: 17.0,
-                  color: Colors.black,
-                ),
+            ),
+            const Text(
+              'Com esta ferramenta, você pode denunciar casos\nde violência  doméstica  de maneira camuflada,\nrápida e segura.',
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.black,
               ),
-              const Text(
-                'Agora um breve tutorial das funcionalidades\nbasicas do app:',
-                style: TextStyle(
-                  fontSize: 17.0,
-                  color: Colors.black,
-                ),
+            ),
+            const Text(
+              'Agora um breve tutorial das funcionalidades\nbasicas do app:',
+              style: TextStyle(
+                fontSize: 17.0,
+                color: Colors.black,
               ),
-              Expanded(
+            ),
+            Expanded(
                 child: Image.asset(
-                  'assets/images/mulheres.png',
-                )
-              ),
-              Row(
-                children: [
-                  const Expanded(
-                    child: Icon(
+              'assets/images/mulheres.png',
+              height: 300.0,
+              width: 300.0,
+            )),
+            Row(
+              children: [
+                const Expanded(
+                  child: Icon(
                     Icons.arrow_back_rounded,
                     color: Color.fromARGB(255, 104, 104, 104),
                     size: 10.0,
-                    ),
                   ),
-                  const Icon(
-                    Icons.brightness_1,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    size: 10.0,
-                  ),
-                  const Icon(
-                    Icons.panorama_fish_eye,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    size: 10.0,
-                  ),
-                  const Icon(
-                    Icons.panorama_fish_eye,
-                    color: Color.fromARGB(255, 0, 0, 0),
-                    size: 10.0,
-                  ),
-                  Expanded(
-                    child: IconButton(
+                ),
+                const Icon(
+                  Icons.brightness_1,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  size: 10.0,
+                ),
+                const Icon(
+                  Icons.panorama_fish_eye,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  size: 10.0,
+                ),
+                const Icon(
+                  Icons.panorama_fish_eye,
+                  color: Color.fromARGB(255, 0, 0, 0),
+                  size: 10.0,
+                ),
+                Expanded(
+                  child: IconButton(
                       icon: const Icon(Icons.arrow_forward_rounded),
-                      onPressed: () {Navigator.of(context).pushNamed('/tutrial2',);}
-                    ),
-                  ),
-                ],
-              ),
-            ]
-          )
-        ),
-      )
-    );
+                      onPressed: () {
+                        Navigator.of(context).pushNamed(
+                          '/tutorial2',
+                        );
+                      }),
+                ),
+              ],
+            ),
+          ])),
+        ));
   }
 }
